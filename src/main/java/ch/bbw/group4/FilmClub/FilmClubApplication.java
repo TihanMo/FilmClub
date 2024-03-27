a47a797458348242717a4a8decdb0207733afac2
Tihan Samiul Ahmmed <tihan@DESKTOP-B07OV07.localdomain>
2024-03-27 09:23:14 +0100

    Spring App inizialized

---
+++ src/main/java/ch/bbw/group4/FilmClub/FilmClubApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.group4.FilmClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/group4/FilmClub/FilmClubApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.group4.FilmClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
