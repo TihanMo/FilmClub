079e465bb05e8ca6116975cf18ceb580cbf678fd
klionCH <sven.luebcke@lernende.bbw.ch>
2024-03-27 09:12:48 +0100

    wechsel von gradle zu maven

---
+++ filmprojekt/filmprojekt/src/main/java/ch/bbw/LGTS/filmprojekt/FilmprojektApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.LGTS.filmprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmprojektApplication.class, args);
+	}
+
+}
---
+++ filmprojekt/filmprojekt/src/test/java/ch/bbw/LGTS/filmprojekt/FilmprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.LGTS.filmprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
