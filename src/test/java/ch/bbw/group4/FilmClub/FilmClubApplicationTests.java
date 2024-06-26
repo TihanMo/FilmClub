7ee61682cb68d35bd0797836888ad7b8318334b4
AimceptionGian <67605487+AimceptionGian@users.noreply.github.com>
2024-06-26 09:54:03 +0200

    Testklasse angefangen :)

--- src/test/java/ch/bbw/group4/FilmClub/FilmClubApplicationTests.java
+++ src/test/java/ch/bbw/group4/FilmClub/FilmClubApplicationTests.java
@@ -1,8 +1,24 @@
 package ch.bbw.group4.FilmClub;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FilmClubApplicationTests {
+    private Regisseur regisseur1, regisseur2, regisseur3, regisseur4;
 
+    @BeforeEach
+    public void setUp() {
+        regisseur1 = new Regisseur("Max Mustermann");
+        regisseur2 = new Regisseur("Max1 Mustermann1");
+        regisseur3 = new Regisseur("import.org.junit.jupiter.api.*");
+        regisseur4 = new Regisseur("");
+    }
+
+    @Test
+    public void testString() {
+        assertEquals(regisseur1.getString(), "Max Mustermann");
+        assertEquals(regisseur2.getString(), "Max1 Mustermann1");
+        assertEquals(regisseur3.getString(), "import.org.junit.jupiter.api.*");
+        assertNull(regisseur4.getString(), null);
+    }
 }
